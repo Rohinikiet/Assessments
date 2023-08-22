f05d2ba339cf4b012753af202c93f64708649f25
Rohinikiet <rohoniudi@gmail.com>
2023-08-22 15:40:53 +0530

    change code

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,9 @@
 def is_symmetrical(s):
-    return s == s[::-1]
+     mid = len(s) // 2
+     return s[:mid] == s[mid:][::-1]
 
 def is_palindrome(s):
-    mid = len(s) // 2
-    return s[:mid] == s[mid:][::-1]
+   return s == s[::-1]
 
 input_string = input("Enter a string: ")
 
